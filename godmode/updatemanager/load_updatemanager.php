8cea1539bb58bac325cf6a1be1359c75c709abdb
Esteban Sánchez <esteban.sanchez@artica.es>
2008-12-16 09:54:35 +0000

    2008-12-16  Esteban Sanchez  <estebans@artica.es>
            * godmode/updatemanager/load_updatemanager.php: Definetely fixed user
            and incident counts.

--- godmode/updatemanager/load_updatemanager.php
+++ godmode/updatemanager/load_updatemanager.php
@@ -59,10 +59,8 @@ function get_user_key ($settings) {
 	   We only want to know this for statistics records.
 	   Feel free to disable this extension if you want.
 	 */
-	$u = (int) get_db_value ('COUNT(`id_usuario`)', 'tusuario',
-		'id_usuario', 'id_usuario');
-	$i = (int) get_db_value ('COUNT(`id_incidencia`)', 'tincidencia',
-		'id_incidencia', 'id_incidencia');
+	$u = (int) get_db_sql ('SELECT COUNT(`id_usuario`) FROM tusuario');
+	$i = (int) get_db_sql ('SELECT COUNT(`id_incidencia`) FROM tincidencia');
 	$user_key = array ('U' => $u, 'I' => $i);
 	
 	return json_encode ($user_key);
