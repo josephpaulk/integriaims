6d1c915e35d42a191c780c83bfec80b6449d7cef
Jorge González <jorgegonz@artica.es>
2009-01-30 12:09:38 +0000

    2009-01-30  Jorge Gonzalez  <jorgegonz@artica.es>
            * include/help/en/help_no_wu_completion.php: fixed wrong header.
    
            * include/help/es, include/help/es/help_profiles.php,               
              include/help/es/help_hours_perday.php,                            
              include/help/es/help_notification_period.php,                     
              include/help/es/help_limit_size.php,                              
              include/help/es/help_autowu_completion.php,                       
              include/help/es/help_no_wu_completion.php: Added Spanish help
              files.

--- include/help/en/help_no_wu_completion.php
+++ include/help/en/help_no_wu_completion.php
@@ -1,4 +1,4 @@
-<h1>Auto Workunit Completion</h1>
+<h1>No WU completion users</h1>
 
 <p>
 This is a list of the users that do not have automatic workunit completion. Each user must be separated by a blank space " ".
