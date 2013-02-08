5172fc1b8a46310b072e9701dce58c54b7d94f41
Vanessa Gil <vanessa.gil@artica.es>
2013-02-08 09:43:27 +0000

    2013-02-08  Vanessa Gil <vanessa.gil@artica.es>
    	* operation/incidents/incident_type_filed.php: Changed
    	help tip.

--- operation/incidents/incident_type_field.php
+++ operation/incidents/incident_type_field.php
@@ -56,7 +56,7 @@ $types = array('text' =>__('Text'), 'textarea' => __('Textarea'), 'combo' => __(
 $table->data[0][1] = print_label (__("Type"), "label-id", 'text', true);
 $table->data[0][1] .= print_select ($types, 'type', $type, '', __('Select type'), '0', true);
 
-$table->data[1][0] = print_input_text ('combo_value', $combo_value, '', 45, 100, true, __('Combo value')).'<div id=tip_hidden>'. print_help_tip (__("If you choose 'combo' option, you must added combo values separated by comma"), true).'</div>';
+$table->data['id_combo_value'][0] = print_input_text ('combo_value', $combo_value, '', 45, 100, true, __('Combo value')).print_help_tip (__("Set values separated by comma"), true);
 
 echo '<form method="post" action="index.php?sec=incidents&sec2=operation/incidents/type_detail&id='.$id_incident_type.'&add_field=1">';
 print_table ($table);
@@ -79,24 +79,18 @@ echo '</form>';
 <script  type="text/javascript">
 $(document).ready (function () {
 	if ($("#type").val() == "combo") {
-		$("#label-text-combo_value").css ('display', '');
-		$("#text-combo_value").css ('display', '');
-		$("#tip_hidden").css ('display', '');
+		$("#table1-id_combo_value-0").css ('display', '');
 	} else {
-		$("#label-text-combo_value").css ('display', 'none');
-		$("#text-combo_value").css ('display', 'none');
-		$("#tip_hidden").css ('display', 'none');
+		$("#table1-id_combo_value-0").css ('display', 'none');
 	}
 
 });
 
 $("#type").change (function () {
 	if ($("#type").val() == "combo") {
-		$("#label-text-combo_value").css ('display', '');
-		$("#text-combo_value").css ('display', '');
+		$("#table1-id_combo_value-0").css ('display', '');
 	} else {
-		$("#label-text-combo_value").css ('display', 'none');
-		$("#text-combo_value").css ('display', 'none');
+		$("#table1-id_combo_value-0").css ('display', 'none');
 	}
 });
 </script>
