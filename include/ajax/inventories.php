35f6efc35053d51f061ed3dd796f29a35ab4565a
Vanessa Gil <vanessa.gil@artica.es>
2013-04-15 15:24:57 +0000

    2013-04-15  Vanessa Gil <vanessa.gil@artica.es>
    	* include/ajax/inventories.php: Changed pop up
    	with external table view.

--- include/ajax/inventories.php
+++ include/ajax/inventories.php
@@ -7,6 +7,14 @@ $get_external_data = get_parameter('get_external_data');
 if ($get_external_data) {
 	$table_name = get_parameter('table_name');
 	
+	$sql_ext = "SHOW COLUMNS FROM ".$table_name;
+	$desc_ext = get_db_all_rows_sql($sql_ext);
+	
+	$fields = array();
+	foreach ($desc_ext as $key=>$ext) {
+		$fields[$ext['Field']] = $ext['Field'];
+	}
+	
 	$external_data = get_db_all_rows_in_table($table_name);
 	
 	if ($external_data !== false) {
@@ -16,29 +24,31 @@ if ($get_external_data) {
 		$table->data = array ();
 		$table->head = array ();
 		
-		$keys = array_keys($external_data[0]);
+		$keys = array_keys($fields);
 		
 		$i = 0;
 		foreach ($keys as $k=>$head) {
 			$table->head[$i] =$head;
 			$i++;
 		}
+		
+		foreach ($external_data as $key => $ext_data) {
+			$j = 0;
+			foreach ($ext_data as $k => $dat) {
 
-	foreach ($external_data as $key => $ext_data) {
-
-		$data_values = array_values($ext_data);
-
-		foreach ($data_values as $k => $dat) {
-			$data[$k] = $dat;
+				if (array_key_exists($k, $fields)) {
+					$data[$j] = $dat;
+				}
+				$j++;
+			}
+			array_push ($table->data, $data);
 		}
-		array_push ($table->data, $data);
+
+		print_table ($table);
+	} else {
+		echo "<h4>".__("No data to show")."</h4>";
 	}
-	
-	print_table ($table);
-} else {
-	echo "<h4>".__("No data to show")."</h4>";
-}
-return;
+	return;
 }
 
 ?>
