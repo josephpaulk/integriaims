a79b873e5882cab9469212185f086540d57958fe
Alejandro Gallardo Escobar <alejandro.gallardo@artica.es>
2013-07-26 10:16:43 +0000

    2013-07-26  Alejandro Gallardo <alejandro.gallardo@artica.es>
    	* operation/invoices/invoice_template.php: Description
    	id now a tooltip of an icon on the right of "Invoice ID".
    	Visual changes.

--- operation/invoices/invoice_template.php
+++ operation/invoices/invoice_template.php
@@ -6,6 +6,9 @@
 		<tr>
 			<td style="text-align:left; font-size:16px; color:black;">
 				<?php echo '<b>'.__('Invoice ID'). ':</b> '.$invoice['bill_id'] ?>
+				<?php if ($clean_output != 1) {
+					echo '&nbsp;<img src="images/page_white_text.png" title="'.$invoice['description'].'">';
+				} ?>
 			</td>
 			<td style="font-size:16px; color:black;">
 				<?php echo '<b>'.__('Issue date'). ':</b> '.$invoice['invoice_create_date'] ?>
@@ -21,27 +24,24 @@
 	</table>
 	<table style="padding-left:20px; width:575px;">
 		<tr>
-			<td>
+			<td style="padding-right:20px;">
 				<div style="padding-left:4px; padding-right:4px;">
 					<?php echo '<div align="center"><b style="font-size:16px; color:white;">C</b><br><br></div>' ?>
-					<?php echo '<div style="font-size:16px;padding-top:4px; padding-bottom:4px;">'.$company_from['name'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_from['fiscal_id'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_from['address'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_from['country'].'</div>' ?>
+					<?php echo '<div style="font-size:14px;padding-top:2px; padding-bottom:2px;"><pre>'.$config["invoice_header"].'</pre></div>' ?>
 				</div>
 			</td>
-			<td>
+			<td style="padding-left:20px;">
 				<div style="padding-left:4px; padding-right:4px;">
 					<?php echo '<div align="center"><b style="font-size:16px; color:black;">'.__('Customer').'</b><br><br></div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_to['name'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_to['fiscal_id'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_to['address'].'</div>' ?>
-					<?php echo '<div style="font-size:16px; padding-top:4px; padding-bottom:4px;">'.$company_to['country'].'</div>' ?>
+					<?php echo '<div style="font-size:14px; padding-top:2px; padding-bottom:2px;">'.$company_to['name'].'</div>' ?>
+					<?php echo '<div style="font-size:14px; padding-top:2px; padding-bottom:2px;">'.$company_to['fiscal_id'].'</div>' ?>
+					<?php echo '<div style="font-size:14px; padding-top:2px; padding-bottom:2px;">'.$company_to['address'].'</div>' ?>
+					<?php echo '<div style="font-size:14px; padding-top:2px; padding-bottom:2px;">'.$company_to['country'].'</div>' ?>
 				</div>
 			</td>
 		</tr>
 	</table>
-	<table style="border-top: 2px solid black; border-bottom:1px solid black; width:620px;">
+	<table style="border-top: 2px solid black; width:620px;">
 		<tr>
 			<td style="padding:15px; text-align:left; font-size:16px; color:black;">
 				<?php echo '<b>'.__('Concept').'</b>' ?>
@@ -54,45 +54,40 @@
 		
 		if ($invoice['concept1'] != "") {
 			echo '<tr>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:left;">'.$invoice['concept1'].'</td>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:right;">'
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:left;">'.$invoice['concept1'].'</td>';
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:right;">'
 					.$invoice['amount1'].' '.$invoice['currency'].'</td>';
 			echo '</tr>';
 		}
 		if ($invoice['concept2'] != "") {
 			echo '<tr>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:left;">'.$invoice['concept2'].'</td>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:right;">'
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:left;">'.$invoice['concept2'].'</td>';
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:right;">'
 					.$invoice['amount2'].' '.$invoice['currency'].'</td>';
 			echo '</tr>';
 		}
 		if ($invoice['concept3'] != "") {
 			echo '<tr>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:left;">'.$invoice['concept3'].'</td>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:right;">'
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:left;">'.$invoice['concept3'].'</td>';
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:right;">'
 					.$invoice['amount3'].' '.$invoice['currency'].'</td>';
 			echo '</tr>';
 		}
 		if ($invoice['concept4'] != "") {
 			echo '<tr>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:left;">'.$invoice['concept4'].'</td>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:right;">'
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:left;">'.$invoice['concept4'].'</td>';
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:right;">'
 					.$invoice['amount4'].' '.$invoice['currency'].'</td>';
 			echo '</tr>';
 		}
 		if ($invoice['concept5'] != "") {
 			echo '<tr>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:left;">'.$invoice['concept5'].'</td>';
-				echo '<td style="padding:5px 15px 15px 15px; font-size:16px; text-align:right;">'
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:left;">'.$invoice['concept5'].'</td>';
+				echo '<td style="padding:5px 15px 15px 15px; font-size:14px; text-align:right;">'
 					.$invoice['amount5'].' '.$invoice['currency'].'</td>';
 			echo '</tr>';
 		}
 		?>
-	</table>
-	<table style="padding-top:15px; padding-bottom:15px; width:620px;">
-		<tr>
-			<td><?php echo $invoice['description'] ?></td>
-		</tr>
 	</table>
 	<table style="border-top:2px solid black; border-bottom:1px solid black; width:620px; text-align:right;">
 		<tr>
