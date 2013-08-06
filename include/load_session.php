d11a348fc3ae1f130c2c7b68618242a6e91e85e0
Darío Rodríguez <drd.sqki@gmail.com>
2013-08-06 08:57:41 +0000

    2013-08-06  Dario Rodriguez <dario@artica.es>
            * include/load_session.php: Added header.
            * include/config_process.php: Fixed a require           
            once function.
    
            #This commit was made from internal site 
            # and I do not own the propety or any
            # attribution from it.

--- include/load_session.php
+++ include/load_session.php
@@ -1,5 +1,19 @@
 <?php   
 
+// INTEGRIA - the ITIL Management System
+// http://integria.sourceforge.net
+// ==================================================
+// Copyright (c) 2012 Ártica Soluciones Tecnológicas
+// http://www.artica.es  <info@artica.es>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 function mysql_session_open ($save_path, $session_name) {
 
 	return true;
