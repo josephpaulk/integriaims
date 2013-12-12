20489c6d8588b157a1ab29d1a7a6b599bca26d3c
Miguel De Dios <tres.14159@gmail.com>
2013-12-12 13:01:03 +0000

    2013-12-12 Miguel de Dios <miguel.dedios@artica.es>
    	* *: first version copied from the "pandora android console" from
    	the open source project "Pandora FMS".

---
+++ extras/integria_android_console/gen/integria/integriaandroidconsole/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package integria.integriaandroidconsole;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ extras/integria_android_console/src/integria/integriaandroidconsole/ConnectionCustomToast.java
@@ -0,0 +1,54 @@
+/*
+Integria - http://integriaims.com/
+
+==================================================
+Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+Please see http://integriaims.com/ for full contribution list
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; version 2
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
+/**
+ * Activity of help view.
+ * 
+ * @author Miguel de Dios Matías
+ * 
+ */
+
+package integria.integriaandroidconsole;
+
+import integria.integriaandroidconsole.R;
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ConnectionCustomToast extends Activity {
+	private AnimationDrawable loadAnimation;
+	public static Activity activity;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Hack to close the activity from other activity.
+		activity = this;
+		
+		setContentView(R.layout.connection_custom_toast);
+		
+		ImageView loading_anim = (ImageView)findViewById(R.id.loading_anim);
+		loading_anim.setImageResource(R.drawable.loading);
+		loadAnimation = (AnimationDrawable)loading_anim.getDrawable();
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus){
+		loadAnimation.start();
+	}
+}
