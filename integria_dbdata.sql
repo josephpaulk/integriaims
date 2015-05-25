72af88f3c025b49b08ed9b809f1809641feff117
Vanessa Gil <vanessa.gil@artica.es>
2015-05-25 14:53:26 +0000

    2015-05-25  Vanessa Gil <vanessa.gil@artica.es>
    	* integria_dbdata.sql: Update token.

--- integria_dbdata.sql
+++ integria_dbdata.sql
@@ -53,7 +53,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('license', 'INTEGRIA-FREE'),
 ('login_background', 'flowers.jpg'),
 ('inventory_default_owner', 'admin'),
-('minor_release', 20);
+('minor_release', 21);
 
 -- Default password is 'integria'
 
