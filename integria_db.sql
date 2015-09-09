56b8cee272635fb46277afdc31a956531702c557
Alejandro Gallardo Escobar <alejandro.gallardo@artica.es>
2015-09-09 14:57:40 +0000

    2015-09-09  Alejandro Gallardo <alejandro.gallardo@artica.es>
    	* integria_db.sql,
    	  integria_dbdata.sql: Updated with the data of the new
    	MR27.

--- integria_db.sql
+++ integria_db.sql
@@ -1288,3 +1288,25 @@ CREATE TABLE `tattachment_track` (
   `data` text NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---------------------------------------------------------------------
+-- Table tattachment_track (09/09/2015)
+-- ---------------------------------------------------------------------
+CREATE TABLE `ttag` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `colour` ENUM('blue', 'grey', 'green', 'yellow', 'orange', 'red') DEFAULT 'orange',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---------------------------------------------------------------------
+-- Table tattachment_track (09/09/2015)
+-- ---------------------------------------------------------------------
+CREATE TABLE `tlead_tag` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `tag_id` bigint(20) unsigned NOT NULL,
+  `lead_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`tag_id`) REFERENCES ttag(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`lead_id`) REFERENCES tlead(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- integria_dbdata.sql
+++ integria_dbdata.sql
@@ -53,7 +53,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('license', 'INTEGRIA-FREE'),
 ('login_background', 'flowers.jpg'),
 ('inventory_default_owner', 'admin'),
-('minor_release', 26);
+('minor_release', 27);
 
 -- Default password is 'integria'
 
