946780cdabea84b9e639f6e123ae3d47bc21d411
Vanessa Gil <vanessa.gil@artica.es>
2015-12-09 12:00:03 +0100

    Added new macros.

--- include/help/en/help_macros_workflow.php
+++ include/help/en/help_macros_workflow.php
@@ -7,21 +7,30 @@ Following strings will be replaced on runtime on the templates who use it:
 
 <p>
 
-<li>_sitename_: Site name, as defined in setup.
-<li>_incident_title_: Title of the ticket.
-<li>_username_: Name of the user who receive the mail (login name)</li>
-<li>_fullname_: Fullname of the user who receive the mail</li>
-<li>_incident_id_: ID of ticket.
-<li>_access_url_: Incident URL
-<li>_creation_timestamp_: Date/Time of ticket creation.
-<li>_update_timestamp_: Last time ticket was updated.
-<li>_owner_: User who manages the ticket.
-<li>_group_: Group assigned to this ticket.
-<li>_author_: Creator of ticket.
-<li>_priority_: Ticket priority.
-<li>_status_: Status of the ticket.
-<li>_resolution_: Resolution of the ticket.
+<li>_sitename_: Site name, as defined in setup.</li>
+<li>_incident_title_: Title of the ticket.</li>
+<li>_username_: Name of the user who receive the mail (login name).</li>
+<li>_fullname_: Fullname of the user who receive the mail.</li>
+<li>_incident_id_: ID of ticket.</li>
+<li>_access_url_: Incident URL.</li>
+<li>_creation_timestamp_: Date/Time of ticket creation.</li>
+<li>_update_timestamp_: Last time ticket was updated.</li>
+<li>_owner_: User who manages the ticket.</li>
+<li>_group_: Group assigned to this ticket.</li>
+<li>_author_: Creator of ticket.</li>
+<li>_priority_: Ticket priority.</li>
+<li>_status_: Status of the ticket.</li>
+<li>_resolution_: Resolution of the ticket.</li>
+<li>_incident_own_email_: Owner's email.</li>
+<li>_incident_gr_email_: Group's email.</li>
+<li>_incident_creator_email_: Author's email.</li>
 
+</p>
+
+<p>
+<b>Example To:</b>
+<br>
+_incident_own_email_
 </p>
 
 <p>
