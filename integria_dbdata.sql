3c412a88f367776f5a7fc525fa3eb1f3983cbdcf
daniel <daniel.barbero@artica.es>
2016-03-01 09:35:15 +0100

    update package

--- integria_dbdata.sql
+++ integria_dbdata.sql
@@ -48,7 +48,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('max_days_fs_files', 7),
 ('max_days_files_track', 15),
 ('update_manager_installed', 1),
-('current_package', 45),
+('current_package', 46),
 ('url_updatemanager', 'https://artica.es/integriaupdate4/server.php'),
 ('license', 'INTEGRIA-FREE'),
 ('login_background', 'flowers.jpg'),
