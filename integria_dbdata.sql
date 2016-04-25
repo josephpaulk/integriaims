6e94e4648c8727eca8ee9ad1c14cf2cac7e796cc
Vanessa Gil <vanessa.gil@artica.es>
2016-04-25 15:57:16 +0200

    Hide menus wiki, people and customers.

--- integria_dbdata.sql
+++ integria_dbdata.sql
@@ -53,7 +53,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('license', 'INTEGRIA-FREE'),
 ('login_background', 'flowers.jpg'),
 ('inventory_default_owner', 'admin'),
-('minor_release', 44);
+('minor_release', 45);
 
 -- Default password is 'integria'
 
