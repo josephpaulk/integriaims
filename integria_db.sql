8aeb07cd2ea28b98cc93733a9363c0cef8cee2f6
daniel <daniel.barbero@artica.es>
2016-05-25 09:00:18 +0200

    create new tables for custom field in company

--- integria_db.sql
+++ integria_db.sql
@@ -1394,3 +1394,34 @@ CREATE TABLE `treport_subtype` (
   `nombre`  mediumtext NOT NULL,
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---------------------------------------------------------------------
+-- Table tcompany_field (24/05/2016)
+-- ---------------------------------------------------------------------
+
+CREATE TABLE `tcompany_field` ( 
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT, 
+  `label` varchar(100) NOT NULL DEFAULT '', 
+  `type` enum('text', 'textarea', 'combo', 'linked', 'numeric', 'date') DEFAULT 'text', 
+  `combo_value` text,
+  `parent` mediumint(8) unsigned default 0,
+  `linked_value` LONGTEXT default NULL,
+  `order` mediumint(8) unsigned default 0, 
+  PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---------------------------------------------------------------------
+-- Table tcompany_field_data (24/05/2016)
+-- ---------------------------------------------------------------------
+
+CREATE TABLE `tcompany_field_data` ( 
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, 
+  `id_company` mediumint(8) unsigned NOT NULL,
+  `id_company_field` mediumint(8) unsigned NOT NULL, 
+  `data` text, 
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_company_field`) REFERENCES tcompany(`id`)
+          ON DELETE CASCADE,
+    FOREIGN KEY (`id_company`) REFERENCES tcompany(`id`)
+          ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- integria_dbdata.sql
+++ integria_dbdata.sql
@@ -53,7 +53,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('license', 'INTEGRIA-FREE'),
 ('login_background', 'paisaje.jpg'),
 ('inventory_default_owner', 'admin'),
-('minor_release', 46);
+('minor_release', 47);
 
 -- Default password is 'integria'
 
