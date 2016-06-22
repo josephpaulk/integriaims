5be92253063927b09d01c599c1978faa94c20e0a
fermin831 <fermin.hernandez@artica.es>
2016-06-22 13:57:33 +0200

    Solved problem with Any groups in projects display Tickets #3585

--- operation/projects/project_overview.php
+++ operation/projects/project_overview.php
@@ -212,6 +212,7 @@ if ($search_id_project_group > -1)
 else
 	$apertura = 'closed';
 echo "<div class='divresult'>";
+$total_projects = 0;
 foreach($project_groups as $group) {
 	$info_general = "";
 	// Get projects info
@@ -237,6 +238,7 @@ foreach($project_groups as $group) {
 	$projects = $aux_projects;
 	
 	$nprojects = count($projects);
+	$total_projects += $nprojects;
 	if ($nprojects > 0 ) {
 		$info_general .= "<tr>";
 		//$info_general .= "<td class='no_border size_min'></td>";
@@ -318,10 +320,10 @@ foreach($project_groups as $group) {
 		}
 		$title = "<img src='images/project_groups_small/" . $group["icon"] . "' style= 'float: left;'><a href='index.php?sec=projects&sec2=operation/projects/project_overview&search_id_project_group=".$group["id"]."'>".$group["name"]."</a>&nbsp; | &nbsp;".__('Nº Projects').": ".$nprojects;
 		print_container('info_projects_'.$group["id"], $title, $info_general, $apertura, false, '10px', '', '', 6, 'no_border_bottom');
-	} else {
-		ui_print_error_message(__('There are not projects to show.'));
-		return;
 	}
+}
+if ($total_projects == 0) {
+	ui_print_error_message(__('There are not projects to show.'));
 }
 echo "</div>";
 ?>
